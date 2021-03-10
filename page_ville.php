4b9ebaa32a29c53085c1fc31b8c95f6a34865283
mame-fatou <60789537+mame-fatou@users.noreply.github.com>
2021-03-10 23:22:02 +0100

    Add files via upload
    Page ville

---
+++ page_ville.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<TITLE> ville </TITLE>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</HEAD>
+
+ville
+<SELECT name="ville">
+<OPTION
+VALUE="Marne-la-vallée"> Marne-la-vallée </OPTION>
+</SELECT>
+
+</HTML>
