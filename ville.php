be4030ff62fe03ea1df520f862059f87cea46c45
mame-fatou <60789537+mame-fatou@users.noreply.github.com>
2021-04-01 16:37:02 +0200

    Add files via upload

--- ville.php
+++ ville.php
@@ -1,36 +1,91 @@
 <?php include 'bd.php' ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style/style_page1.css" type="text/css" media="screen" />
 
-    <title>Villes </title>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+
+    <title>Page Villes </title>
 </head>
 <body>
-   <h1 class="titre">page des villes </h1>    
 
-   <?php
-    $bdd = getBD();
-    
-    $rep = $bdd->query('select * from villes where ID_V ='.$_REQUEST['ID_V']);
-     
-    $article = $rep -> fetch()
-    ?>
-        
-   
-    </div>
-    <ol>
-      <li>Nom ville: <?php echo $article['nom_ville'] ?></li>
-      <li>Département: <?php echo $article['departement'] ?></li>
-      <li>Population: <?php echo $article['population'] ?> £</li>
-	  <li>taux de chomage: <?php echo $article['taux_chomage'] ?></li><br>
+<style type="text/css">
 
-    </ol>
+
+ul{
+	position:relative;
+}
+img{
+	position:absolute;
+	top:0px;
+	left:100px;
+
+
+}
+
+
+ul li:hover img{
+
+z-index: 1;
+}
+</style>
+
+   <h1 class="titre">Villes </h1>  
    
-      <p>
-        <a href="Acceuil.php">Retour à la page d'acceuil </a>
-    </p>
+   
+<p class="comentaire"> Choisissez la ville qui vous intéresse afin de voir toutes les gares qui s'y trouvent et d'obtenir des informations qui vous interessent</p></br>   
+
+
+<ul>
+  <li><img width = 300px src="images/paris.jpg" /><img width = 300px src="images/Lille.jpg" /></li>
+  <li><img width = 300px src="images/Montpellier.jpg" /><img width = 300px src="images/strasbourg.jpg" /></li>
+  <li><img width = 300px src="images/Bordeau.jpg" /><img width = 300px src="images/lyon.jpg" /></li>
+  <li><img width = 300px src="images/Reims.jpg" /><img width = 300px src="images/nice.jpg" /></li>
+  <li><img width = 300px src="images/toulouse.jpg" /><img width = 300px src="images/paris.jpg" /></li>
+  <li><img width = 300px src="images/Toulon.jpg" /><img width = 300px src="images/dijon.jpg" /></li>
+  <li><img width = 300px src="images/nantes.jpg" /><img width = 300px src="images/toulouse.jpg" /></li>
+  <li><img width = 300px src="images/marseilles.jpg" /><img width = 300px src="images/villes.jpg" /></li>
+  <li><img width = 300px src="images/Grenoble.jpg" /><img width = 300px src="images/montpellier.jpg" /></li>
+
+</ul>
+
+
+
+
+
+
+
+  <form method="get" action="gare.php" autocomplete="off">
+
+
+ <?php
+
+
+$bdd= new PDO('mysql:host=localhost;dbname=projetgares;charset=utf8', 'root', 'root');
+$rep = $bdd->query('select * from ville');
+echo "<table class= cMonTableau '>";
+echo "<tr>";
+
+echo "<th>Ville choisie : </th>";
+
+echo "<th></th>";
+echo "</tr>";
+while ($line = $rep ->fetch()) {
+echo "<tr>";
+
+echo "<th><a href='gare.php?ville=".$line["nom"]."'>".$line["nom"]."</a></th>";
+
+echo "</tr>";
+}
+echo "</table>";
+$rep ->closeCursor();
+?>
+ 
+      <p><a href="profil.php">Mon Profil </a></p>
+	  	<p><a href="ville.php">Retour à la page ville</a></p>
+        <p><a href="accueil.php">retour à la page d'accueil </a></p>
   
 </body>
 </html>
